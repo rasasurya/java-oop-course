034da97d44af3110fb4e1bd1b33d3c72789998a7
Suryadi <suryadi@accelbyte.net>
2021-04-03 21:25:47 +0700

    Update main with Scanner

--- Pertemuan3/src/app/MainApp.java
+++ Pertemuan3/src/app/MainApp.java
@@ -1,11 +1,16 @@
 package app;
 
+import java.util.Scanner;
+
+
 //import data.Admin;
 //import data.Users;
 
 import polymorph.*;
 
 public class MainApp {
+
+	private static Scanner masuk;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -21,12 +26,17 @@ public class MainApp {
 //		System.out.println("Your new password: " + limidev.getPassword());
 		
 		Human man = new Human("Kiki");
+		var masukan = new Scanner(System.in);
+		
+		System.out.println("Masukkan Nama: " );
+		man.name = masukan.nextLine();
 		man.iam();
 		
 		Student student = new Student("Sinta", "18725234");
 		student.iam();
 		
 		Employee employee = new Employee("Kumar");
+		System.out.println(employee.toString());
 		employee.iam();
 		
 		who(man);
